6487fae7d43d15f6385ade7857cf45d068c7dbfa
karanDevX11 <bistakaran89@gmail.com>
2025-05-14 20:41:59 +0545

    Add get result endpoint

--- app/main.py
+++ app/main.py
@@ -19,24 +19,18 @@ def health_check():
 RESULTS_DIR = Path("Results")
 RESULTS_DIR.mkdir(exist_ok=True)
 
-def save_analysis_to_file(analysis: dict):
-    """save analysis result to a file"""
-    analysis_id = str(uuid.uuid4())
-    filepath = RESULTS_DIR/ f"{analysis_id}.json"
-    with open(filepath, "w") as f:
-        json.dump(analysis, f)
-    return analysis_id
-
 @app.post("/upload")
 async def upload_resume(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
     """Endpoint to upload resume files"""
+    analysis_id = str(uuid.uuid4())
     try:
         contents = await file.read()
         analysis = analyze_resume(contents, file.filename)
 
         analysis_id = background_tasks.add_task(
             save_analysis_to_file,
-            analysis
+            analysis,
+            analysis_id
         )
         return {"message": "Analysis started", "id": analysis_id}
     
@@ -45,3 +39,26 @@ async def upload_resume(background_tasks: BackgroundTasks, file: UploadFile = Fi
             content={"error": str(e)},
             status_code=500
         )
+    
+def save_analysis_to_file(analysis: dict, analysis_id: str):
+    """save analysis result to a file"""
+    filepath = RESULTS_DIR/ f"{analysis_id}.json"
+    with open(filepath, "w") as f:
+        json.dump(analysis, f)
+    return analysis_id
+
+    
+@app.get("/result/{analysis_id}")
+def get_result(analysis_id: str):
+    """Get analysis result by id"""
+    filepath = RESULTS_DIR/ f"{analysis_id}.json"
+
+    if not filepath.exists():
+        return JSONResponse(
+            content= {"error": "Analysis not found."},
+            status_code=404
+        )
+    with open(filepath, "r") as f:
+        result = json.load(f)
+    
+    return result
